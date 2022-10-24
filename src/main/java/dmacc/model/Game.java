bfc13b12ca39d9de2cec8a067512fac337097eeb
aheinrichs1 <112419161+aheinrichs1@users.noreply.github.com>
2022-10-23 19:07:08 -0500

    fixed

--- src/main/java/dmacc/model/Game.java
+++ src/main/java/dmacc/model/Game.java
@@ -17,82 +17,14 @@ public class Game {
 	private String winner;
 	
 	public Game(String player1) {
+		super();
 		this.player1 = player1;
 		this.setComputerPlayer();
 		this.determineWinner();
 	}
 	
 	private void determineWinner() {
-		
-
-
-		//Code for debugging
-//		if(player1 == "rock") {
-//			if(computerPlayer == "rock") {
-//				winner = "PLAYER ROCK COM ROCK";
-//			}
-//			else if(computerPlayer == "paper") {
-//				winner = "PLAYER ROCK COM PAPER";
-//			}
-//			else {
-//				winner = "PLAYER ROCK COM SCISSORS";
-//			}
-//		} else if(player1 == "paper") {
-//			if(computerPlayer == "rock") {
-//				winner = "PLAYER PAPER COM ROCK";
-//			}
-//			else if(computerPlayer == "paper") {
-//				winner = "PLAYER PAPER COM PAPER";
-//			}
-//			else {
-//				winner = "PLAYER PAPER COM SCISSORS";
-//			}
-//		} else {
-//			if(computerPlayer == "rock") {
-//				winner = "PLAYER SCISSORS COM ROCK";
-//			}
-//			else if(computerPlayer == "paper") {
-//				winner = "PLAYER SCISSORS COM PAPER";
-//			}
-//			else {
-//				winner = "PLAYER SCISSORS COM SCISSORS";
-//			}
-//		}
-		//Debug 2
-//		if(computerPlayer == "rock") {
-//			if(player1 == "rock") {
-//				winner = "COM ROCK PLAYER ROCK";
-//			}
-//			else if(player1 == "paper") {
-//				winner = "COM ROCK PLAYER PAPER";
-//			}
-//			else {
-//				winner = "COM ROCK PLAYER SCISSORS";
-//			}
-//		} else if(computerPlayer == "paper") {
-//			if(player1 == "rock") {
-//				winner = "COM PAPER PLAYER ROCK";
-//			}
-//			else if(player1 == "paper") {
-//				winner = "COM PAPER PLAYER PAPER";
-//			}
-//			else {
-//				winner = "COM PAPER PLAYER SCISSORS";
-//			}
-//		} else {
-//			if(player1 == "rock") {
-//				winner = "COM SCISSORS PLAYER ROCK";
-//			}
-//			else if(player1 == "paper") {
-//				winner = "COM SCISSORS PLAYER PAPER";
-//			}
-//			else {
-//				winner = "COM SCISSORS PLAYER SCISSORS";
-//			}
-//		}
-		
-		//Code if things worked properly
-		if(player1 == "rock") {
+		if(player1.equalsIgnoreCase("rock")) {
 			if(computerPlayer == "rock") {
 				winner = "Draw";
 			}
@@ -102,7 +34,7 @@ public class Game {
 			else {
 				winner = "Player";
 			}
-		} else if(player1 == "paper") {
+		} else if(player1.equalsIgnoreCase("paper")) {
 			if(computerPlayer == "rock") {
 				winner = "Player";
 			}
@@ -123,9 +55,6 @@ public class Game {
 				winner = "Draw";
 			}
 		}
-		
-		//Print for debugging
-		System.out.println("Player: " + player1 + ", Computer: " + computerPlayer);
 	}
 	
 	private void setComputerPlayer() {
